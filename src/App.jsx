a9707a5f14678d99469d05c768d16c9705368f3a
Asif Ali <asifalieas@gmail.com>
2025-04-16 20:23:41 +0530

    adding sidebar component and basic setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SideBar from './components/SideBar'
+
+function App() {
+  return (
+    <main className='h-screen my-8'>
+      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
+      <SideBar />
+    </main>
+  )
+}
+
+export default App
