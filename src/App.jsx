a578bdbd76c4cb5b25a30e0e01698467f2ddf90a
Asif Ali <asifalieas@gmail.com>
2025-04-16 21:00:54 +0530

    adding styles for the input component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SideBar from './components/SideBar'
 
 function App() {
   return (
-    <main className='h-screen my-8 flex ga-8'>
+    <main className='h-screen my-8 flex gap-8'>
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <SideBar />
       <NewProject />
