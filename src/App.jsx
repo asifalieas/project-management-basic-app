5aaa624e125c774e052885923473af20bc284e6c
Asif Ali <asifalieas@gmail.com>
2025-04-16 20:41:00 +0530

    added NewProject and Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from './components/NewProject'
 import SideBar from './components/SideBar'
 
 function App() {
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex ga-8'>
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <SideBar />
+      <NewProject />
     </main>
   )
 }
