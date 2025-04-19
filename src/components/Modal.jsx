5256188d7b76294404f6c395dd07526694f0938d
Asif Ali <asifalieas@gmail.com>
2025-04-19 16:27:00 +0530

    added modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useImperativeHandle, useRef } from 'react'
+import { createPortal } from 'react-dom'
+
+export default function Modal({ ref, buttonCaption, children }) {
+  const dialog = useRef()
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal()
+      },
+    }
+  })
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method='dialog'>
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  )
+}
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,17 +1,29 @@
 import { useRef } from 'react'
 import Button from './Button'
 import Input from './Input'
+import Modal from './Modal'
 
 export default function NewProject({ onAddProject }) {
   const title = useRef()
   const description = useRef()
   const dueDate = useRef()
 
+  const modal = useRef()
+
   function handleSave() {
     const enteredTitle = title.current.value
     const enteredDescription = description.current.value
     const enetedDueDate = dueDate.current.value
 
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enetedDueDate.trim() === ''
+    ) {
+      modal.current.open()
+      console.log('invalid input')
+      return
+    }
     onAddProject({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,20 +32,29 @@ export default function NewProject({ onAddProject }) {
   }
 
   return (
-    <div className='w-[65rem] mt-16'>
-      <menu className='flex items-center justify-end gap-4 my-4'>
-        <li>
-          <Button>Cancel</Button>
-        </li>
-        <li>
-          <Button onClick={handleSave}>Save</Button>
-        </li>
-      </menu>
-      <div>
-        <Input type='text' ref={title} label='Title' />
-        <Input ref={description} label='Desccription' textArea />
-        <Input type='date' ref={dueDate} label='Due Date' />
+    <>
+      <Modal ref={modal} buttonCaption='close'>
+        <h2>Invalid input</h2>
+        <p>Oops...seems like you forgot to enter a value.</p>
+        <p>
+          Please make sure you provide a valid input value for all the fields.
+        </p>
+      </Modal>
+      <div className='w-[65rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+          <li>
+            <Button>Cancel</Button>
+          </li>
+          <li>
+            <Button onClick={handleSave}>Save</Button>
+          </li>
+        </menu>
+        <div>
+          <Input type='text' ref={title} label='Title' />
+          <Input ref={description} label='Desccription' textArea />
+          <Input type='date' ref={dueDate} label='Due Date' />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
